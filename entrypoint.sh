ed50aea25ad911ac54e66808b50ad18238658ae2
Rignchen <rinchendemort@gmail.com>
2024-07-01 15:15:50 +0200

    feat: change entrypoint to run sw

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/sh -l
 
-# Use INPUT_<INPUT_NAME> to get the value of an input
-GREETING="Hello, $INPUT_WHO_TO_GREET!"
+# Add " -e " before each languages
+EXTENSIONS=$(echo $INPUT_LANG | sed 's/ / -e /g')
 
-# Use workflow commands to do things like set debug messages
-echo "::notice file=entrypoint.sh,line=7::$GREETING"
-
-# Write outputs to the $GITHUB_OUTPUT file
-echo "greeting=$GREETING" >> "$GITHUB_OUTPUT"
+# Run sw
+sw $INPUT_PATH -e $EXTENSIONS
 
 exit 0
