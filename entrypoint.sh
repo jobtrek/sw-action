c528cf3171171f3247f108d4469e1343446190a0
Rignchen <rinchendemort@gmail.com>
2024-07-02 09:49:20 +0200

    fix: eval sw instead of runing it
    using eval allow to use the $EXTENSIONS as multiple arguments instead of a single one

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,11 @@
 EXTENSIONS=$(echo $INPUT_LANG | sed 's/ / -e /g')
 
 # Run sw
-echo "sw $INPUT_PATH -e $EXTENSIONS"
-sw $INPUT_PATH -e $EXTENSIONS
+eval "sw $INPUT_PATH -e $EXTENSIONS"
 
 # Test if the command was successful
+echo "INPUT_LANG: $INPUT_LANG"
+echo "sw $INPUT_PATH -e $EXTENSIONS"
 echo "Test: $(diff -rq test/test test/expected)"
 tree .
 pwd
