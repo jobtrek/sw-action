3380c3d5ac5b635696876284f06c6ee3e17366e8
Rignchen <rinchendemort@gmail.com>
2024-07-02 14:44:05 +0200

    chore: remove debug messages

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,4 @@ EXTENSIONS=$(echo "$INPUT_LANG" | sed 's/,/ -e /g')
 # Run sw
 eval "sw $INPUT_PATH -e $EXTENSIONS"
 
-# Test if the command was successful
-echo "INPUT_LANG: $INPUT_LANG"
-echo "sw $INPUT_PATH -e $EXTENSIONS"
-echo "Test: $(diff -rq test/test test/expected)"
-tree .
-pwd
-
 exit 0
